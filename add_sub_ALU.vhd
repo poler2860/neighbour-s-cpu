4600da036e50a178396bf55e14960b4a2df76238
Asterinos1 <79656046+Asterinos1@users.noreply.github.com>
2023-04-15 19:13:25 +0300

    updated add_sub_ALU.vhd
    updated version.

--- add_sub_ALU.vhd
+++ add_sub_ALU.vhd
@@ -34,13 +34,13 @@ use ieee.std_logic_unsigned.all;
 
 
 entity add_sub_ALU is
-    Port ( A : in  STD_LOGIC_VECTOR (31 downto 0);
-           B : in  STD_LOGIC_VECTOR (31 downto 0);
-           Op : in  STD_LOGIC_VECTOR (3 downto 0);
-           Output : out  STD_LOGIC_VECTOR (31 downto 0);
-	       Zero : out  STD_LOGIC;
-           Cout : out  STD_LOGIC;
-           Ovf : out  STD_LOGIC);
+    Port( A : in  STD_LOGIC_VECTOR (31 downto 0);
+        B : in  STD_LOGIC_VECTOR (31 downto 0);
+        Op : in  STD_LOGIC_VECTOR (3 downto 0);
+        Output : out  STD_LOGIC_VECTOR (31 downto 0);
+	    Zero : out  STD_LOGIC;
+        Cout : out  STD_LOGIC;
+        Ovf : out  STD_LOGIC);
 end add_sub_ALU;
 
 architecture Behavioral of add_sub_ALU is
@@ -49,22 +49,38 @@ SIGNAL tmpOut : STD_LOGIC_VECTOR (32 downto 0); -- vector for calculate Cout. is
 
 begin
 
-
-
-tmpOut <=   (('0' & A) + ('0' & B)) when Op = "0000" else  -- i join a one more bit('0') for being able to do the add sub commands
-            (('0' & A) - ('0' & B)) when Op = "0001" ; 
-				
-Ovf <= '1' when ((A(31) = B(31)) AND ( tmpOut(31) /= A(31)) AND (Op = "0000") ) else -- add nums with same sign but different sign sum
-       '1' when ((A(31)/= B(31)) AND ( tmpOut(31)  = B(31)) AND (Op = "0001") ) else -- sub nums with different sign but the result has the same sign as the subtrahend (5-(-2) = somthing positive)
-       '0';		
-
-Zero <= '1' when tmpOut = "000000000000000000000000000000000" else
-        '0';		 
-			 
-Cout <= tmpOut(32);
-Output <= tmpOut(31 downto 0); 
- 
-  
+	MainProc: process(Op) is
+	begin
+		if Op = "0000" then
+			tmpOut <=(('0' & A) + ('0' & B)); -- i join a one more bit('0') for being able to do the add sub commands
+		elsif Op = "0001" then
+			tmpOut <=  (('0' & A) - ('0' & B)); -- i join a one more bit('0') for being able to do the add sub commands
+		end if;
+	end process MainProc;
+	
+	--check for overflow occurance.
+	CheckOverflow: process(Op) is
+	begin																		
+		if((A(31) = B(31)) AND ( tmpOut(31) /= A(31)) AND (Op = "0000") ) then -- add nums with same sign but different sign sum
+			Ovf <= '1';
+		elsif ((A(31)/= B(31)) AND ( tmpOut(31)  = B(31)) AND (Op = "0001") ) then -- sub nums with different sign but the result has the same sign as the subtrahend (5-(-2) = somthing positive)
+			Ovf <= '1';
+		else 
+			Ovf <='0';
+		end if;
+	end process CheckOverflow;
+	
+	CheckZero: process(Op) is 
+	begin
+		if tmpOut = "000000000000000000000000000000000" then
+			Zero <= '1';
+		else
+			Zero <='0';
+		end if
+	end process CheckZero;
+	
+	Cout <= tmpOut(32);
+	Output <= tmpOut(31 downto 0); 
 
 end Behavioral;
 
