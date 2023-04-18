52fa90b0eec8b3f31cde96237df023a70f68df1a
Polychronis Stamoulis <59158728+poler2860@users.noreply.github.com>
2023-04-18 17:19:45 +0300

    Updated RF

--- RF/register.vhd
+++ RF/register.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date:    20:11:12 04/16/2023 
+-- Create Date:    21:28:14 04/14/2023 
 -- Design Name: 
--- Module Name:    decoder_2_to_4 - Behavioral 
+-- Module Name:    register - Behavioral 
 -- Project Name: 
 -- Target Devices: 
 -- Tool versions: 
@@ -22,42 +22,30 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+--use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity decoder_2_to_4 is
-    Port ( Din : in  STD_LOGIC_VECTOR (1 downto 0);
-		     En : in STD_LOGIC;
-           Dout : out  STD_LOGIC_VECTOR (3 downto 0));
-end decoder_2_to_4;
+entity register is
+    Port ( Rin : in  STD_LOGIC_VECTOR (31 downto 0);
+           Rout : out  STD_LOGIC_VECTOR (31 downto 0);
+           WE : in  STD_LOGIC;
+           CLK : in  STD_LOGIC);
+end register;
 
-architecture Behavioral of decoder_2_to_4 is
+architecture Behavioral of register is
 
 begin
-	process(En, Din)
+
+	process(CLK, WE, Rin)
 	begin
-		case En is
-			when '1' =>
-				if Din = "00" then
-					Dout <= b"0001";
-					
-				elsif Din = "01" then
-					Dout <= b"0010";
-					
-				elsif Din = b"10" then
-					Dout <= b"0100";
-					
-				else
-					Dout <= b"1000";
-				end if;
-			when others =>
-				Dout <= "0000";
-		end case;
+		if rising_edge(CLK) and WE = '1' then
+			Rout <= Rin;
+		end if;
 	end process;
+
 end Behavioral;
 
